8dfe3174bc2f6b56d269e4e2cd6c039ff330342d
Nick Allen <rutherford.t.b@gmail.com>
2021-11-06 18:42:32 -0500

    added textFields

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,6 +1,9 @@
+import { Container, TextField } from "@material-ui/core";
+import { TextareaAutosize } from "@mui/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import './AddMovie.css';
 
 function AddMovie(props) {
     const dispatch = useDispatch();
@@ -13,7 +16,7 @@ function AddMovie(props) {
     });
 
     useEffect(() => {
-        dispatch({type:'FETCH_GENRES'});
+        dispatch({ type: 'FETCH_GENRES' });
     }, []);
 
     const postMovie = (e) => {
@@ -27,52 +30,51 @@ function AddMovie(props) {
             });
     }
 
-    console.log(movie);
-    console.log('genre', genres);
-
-
     return (
         <>
-            <form onSubmit={postMovie}>
-                <input type="text"
-                    placeholder="Title"
-                    className="input-field"
-                    name="title"
-                    value={movie.title}
-                    onChange={(e) => setMovie({ ...movie, title: e.target.value })}
-                />
-                <input type="text"
-                    placeholder="Poster URL"
-                    className="input-field"
-                    name="title"
-                    value={movie.poster}
-                    onChange={(e) => setMovie({ ...movie, poster: e.target.value })}
-                />
-                <textarea type="text"
-                    placeholder="Description"
-                    className="input-field"
-                    name="title"
-                    value={movie.description}
-                    onChange={(e) => setMovie({ ...movie, description: e.target.value })}
-                />
-                <select 
-                    name="genre"
-                    value={movie.id}
-                    onChange={(e) => setMovie({...movie, genre_id: e.target.value})}
-                    required>
-                    <option name="choose" value={0} disabled>Select a Genre</option>
-                    {genres.map((genre, i) => {
-                        console.log(genre.id);
-                        return (
-                        <option 
-                            key={i} 
-                            value={genre.id}
-                            >{genre.name}</option>
-                        );
-                    })}
-                </select>
-                <button type="submit">Add Movie</button>
-            </form>
+                <form id="add-form" onSubmit={postMovie}>
+                    <TextField type="text"
+                        placeholder="Title"
+                        className="input-field"
+                        name="title"
+                        value={movie.title}
+                        onChange={(e) => setMovie({ ...movie, title: e.target.value })}
+                    />
+                    <TextField type="text"
+                        placeholder="Poster URL"
+                        className="input-field"
+                        name="title"
+                        value={movie.poster}
+                        onChange={(e) => setMovie({ ...movie, poster: e.target.value })}
+                    />
+                    <TextField type="text"
+                        placeholder="Description"
+                        className="input-field"
+                        name="title"
+                        value={movie.description}
+                        multiline
+                        rows={4}
+                        maxRows={8}
+                        onChange={(e) => setMovie({ ...movie, description: e.target.value })}
+                    />
+                    <select
+                        name="genre"
+                        value={movie.id}
+                        onChange={(e) => setMovie({ ...movie, genre_id: e.target.value })}
+                        required>
+                        <option name="choose" value={0} disabled>Select a Genre</option>
+                        {genres.map((genre, i) => {
+                            console.log(genre.id);
+                            return (
+                                <option
+                                    key={i}
+                                    value={genre.id}
+                                >{genre.name}</option>
+                            );
+                        })}
+                    </select>
+                    <button type="submit">Add Movie</button>
+                </form>
         </>
     )
 }
