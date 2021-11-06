ab5cf8f749dbc6edae0b7da3c1308e6b6045f23b
Nick Allen <rutherford.t.b@gmail.com>
2021-11-06 11:20:02 -0500

    added back to list button

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,6 +1,16 @@
+import { useHistory } from "react-router";
+
 function MovieDetails(props) {
+    const history = useHistory();
+    
+    const backToList = () => {
+        history.push('/');
+    }
+
     return (
-        <></>
+        <>
+        <button onClick={backToList}>Back to List</button>
+        </>
     )
 }
 
