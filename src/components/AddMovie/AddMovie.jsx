eab97dbfb18128c74254d3022039817225cdc914
Nick Allen <rutherford.t.b@gmail.com>
2021-11-06 15:11:05 -0500

    text inputs set up

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,6 +1,49 @@
-function AddMovie(props){
+import axios from "axios";
+import { useState } from "react";
+
+function AddMovie(props) {
+    const [movie, setMovie] = useState({
+        title: '',
+        poster: '',
+        description: '',
+        genres: []
+    });
+
+    const postMovie = () => {
+        axios.post('/api/movie/add', movie)
+            .then(response => {
+                console.log('POSTED')
+            }).catch(err => {
+                console.log(err);
+            });
+    }
     return (
-        <></>
+        <>
+            <form onSubmit={postMovie}>
+                <input type="text"
+                    placeholder="Title"
+                    className="input-field"
+                    name="title"
+                    value={movie.title}
+                    onChange={(e) => setMovie({ ...movie, title: e.target.value })}
+                />
+                <input type="text"
+                    placeholder="Poster URL"
+                    className="input-field"
+                    name="title"
+                    value={movie.poster}
+                    onChange={(e) => setMovie({ ...movie, poster: e.target.value })}
+                />
+                <textarea type="text"
+                    placeholder="Description"
+                    className="input-field"
+                    name="title"
+                    value={movie.description}
+                    onChange={(e) => setMovie({ ...movie, description: e.target.value })}
+                />
+                <button type="submit">Add Movie</button>
+            </form>
+        </>
     )
 }
 
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,24 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 import MovieDetails from '../MovieDetails/MovieDetails';
+import AddMovie from '../AddMovie/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
+
         <Route path="/details/:id">
-    <MovieDetails /> 
+          <MovieDetails />
+        </Route>
+
+        <Route path="/add">
+          <AddMovie />
         </Route>
 
         {/* Add Movie page */}
