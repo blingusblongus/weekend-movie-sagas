e26ec64655ea8dbf2fcd667c69cd5e68b4944274
Nick Allen <rutherford.t.b@gmail.com>
2021-11-07 08:51:13 -0600

    started styling MovieDetails

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,11 @@ import MovieList from '../MovieList/MovieList'
 import MovieDetails from '../MovieDetails/MovieDetails';
 import AddMovie from '../AddMovie/AddMovie';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
   return (
     <div className="App">
