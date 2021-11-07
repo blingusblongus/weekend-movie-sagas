e1e70b9af7c414d2d0a84c1b3565e09a27c9b048
Nick Allen <rutherford.t.b@gmail.com>
2021-11-06 22:48:11 -0500

    switched paper to cards on MovieList

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,11 +1,11 @@
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
-import { Container, Paper } from "@material-ui/core";
+import { Card } from "@mui/material";
 
 function MovieItem({ movie }) {
     const history = useHistory();
     const dispatch = useDispatch();
-    
+
     const showDetails = () => {
         //send selected movie to selectedReducer
         dispatch({
@@ -13,20 +13,24 @@ function MovieItem({ movie }) {
             payload: movie.id
         });
 
+        //Then navigate to details page
         history.push(`/details/${movie.id}`);
     }
-    
+
     return (
 
-            <Paper>
-        <div>
-            <h3>{movie.title}</h3>
-            <img 
-                src={movie.poster} 
-                alt={movie.title}
-                onClick={showDetails} />
-        </div>
-        </Paper>
+            <div className="card">
+                <Card elevation={4} sx={{borderRadius: '20px'}}>
+                    <div className="inner-card">
+                <h3>{movie.title}</h3>
+                <img
+                    src={movie.poster}
+                    alt={movie.title}
+                    onClick={showDetails} />
+                    </div>
+                </Card>
+            </div>
+        
     )
 }
 
